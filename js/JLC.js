91c0dcb76867e44c8f60183de42d58dc9e9626dc
MCBBS_007 <88782291+MCKN007@users.noreply.github.com>
2023-05-20 20:29:47 +0800

    更新的网站
    更新了

---
+++ js/JLC.js
@@ -0,0 +1,9 @@
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    var set = document.getElementById("Join");
+    set.href = "mqqwpa://im/chat?chat_type=wpa&uin=2231302257&version=1&src_type=web";
+    var set = document.getElementById("Join2");
+    set.href = "mqqwpa://im/chat?chat_type=wpa&uin=2231302257&version=1&src_type=web";
+    var set = document.getElementById("Join3");
+    set.href = "mqqwpa://im/chat?chat_type=wpa&uin=2231302257&version=1&src_type=web";
+    //set.innerText = "手机端";  
+}
